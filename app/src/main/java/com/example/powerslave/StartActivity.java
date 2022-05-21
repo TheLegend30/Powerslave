e3dbf1df900157aac527d6ac56eaba57b807fa86
Oleh Luibichenko <luibichenko.oleg@gmail.com>
2022-05-21 14:16:13 +0300

    Added unfinished ingame xml files

--- app/src/main/java/com/example/powerslave/StartActivity.java
+++ app/src/main/java/com/example/powerslave/StartActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Buttons in StartActivity
     ImageView backArrowStart;
+    ImageView randomRulerNameStart;
+
+    EditText editTextNameStart;
+    EditText editTextSurnameStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +25,28 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
 
         //Assigning values
         backArrowStart = findViewById(R.id.backArrowStart);
+        randomRulerNameStart = findViewById(R.id.randomRulerNameStart);
+        editTextNameStart = findViewById(R.id.editTextNameStart);
+        editTextSurnameStart = findViewById(R.id.editTextSurnameStart);
 
         //OnClick methods
         backArrowStart.setOnClickListener(this);
+        randomRulerNameStart.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == backArrowStart.getId()) {
             super.finish();
+        } else if (view.getId() == randomRulerNameStart.getId()) {
+            int lengthOfArray = getResources().getStringArray(R.array.eyut_names_m).length;
+            int randNumber = new Random().nextInt(lengthOfArray);
+            editTextNameStart.setText(getResources().getStringArray(R.array.eyut_names_m)[randNumber]);
+
+            lengthOfArray = getResources().getStringArray(R.array.eyut_surnames).length;
+            randNumber = new Random().nextInt(lengthOfArray);
+            editTextSurnameStart.setText(getResources().getStringArray(R.array.eyut_surnames)[randNumber]);
         }
     }
 }
